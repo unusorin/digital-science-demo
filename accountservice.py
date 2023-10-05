81fbf8201db95f12739bbb95de69452fcdab5daf
Sorin Badea <sorin.badea91@gmail.com>
2023-10-05 19:24:49 +0300

    Github workflows && Consistent naming

--- accountservice.py
+++ accountservice.py
@@ -9,7 +9,7 @@ from jose import jwt
 from jose.constants import ALGORITHMS
 
 
-class UserService:
+class AccountService:
     _database_engine: Engine
 
     def __init__(self, database_engine: Engine):
@@ -19,50 +19,50 @@ class UserService:
         with Session(self._database_engine) as session:
             return session.scalars(select(models.Account).limit(limit).offset(offset)).all()
 
-    def get(self, user_id: int) -> models.Account:
+    def get(self, account_id: int) -> models.Account:
         with Session(self._database_engine) as session:
-            user = session.scalar(select(models.Account).where(models.Account.id == user_id))
-            if user == None:
+            account = session.scalar(select(models.Account).where(models.Account.id == account_id))
+            if account == None:
                 raise HTTPException(status_code=404, detail="User not found")
-            return user
+            return account
 
     def get_by_name(self, name: str) -> models.Account:
         with Session(self._database_engine) as session:
             return session.scalar(select(models.Account).where(models.Account.name == name))
 
-    def delete(self, user_id: int):
-        user = self.get(user_id)
+    def delete(self, account_id: int):
+        account = self.get(account_id)
         with Session(self._database_engine) as session:
-            session.delete(user)
+            session.delete(account)
             session.commit()
 
     def create(self, name: str):
-        user = models.Account(name=name)
+        account = models.Account(name=name)
         with Session(self._database_engine) as session:
-            session.add(user)
+            session.add(account)
             session.commit()
-            session.refresh(user)
-            return user
+            session.refresh(account)
+            return account
 
-    def update(self, user_id: int, name: str):
-        user = self.get(user_id)
-        user.name = name
+    def update(self, account_id: int, name: str):
+        account = self.get(account_id)
+        account.name = name
         with Session(self._database_engine) as session:
-            session.add(user)
+            session.add(account)
             session.commit()
         pass
 
-    def update_last_login(self, user: models.Account):
-        user.last_login_date = datetime.datetime.now()
+    def update_last_login(self, account: models.Account):
+        account.last_login_date = datetime.datetime.now()
 
         with Session(self._database_engine) as session:
-            session.add(user)
+            session.add(account)
             session.commit()
         pass
 
     def create_access_token(self, name: str) -> str:
         to_encode = {
-            "username": name
+            "name": name
         }
         encoded_jwt = jwt.encode(to_encode, sso.google_sso.client_secret.__str__(), algorithm=ALGORITHMS.HS256)
         return encoded_jwt
@@ -71,11 +71,11 @@ class UserService:
         if session_token is None:
             return None
         payload = jwt.decode(session_token, sso.google_sso.client_secret.__str__(), algorithms=[ALGORITHMS.HS256])
-        username: str = payload.get("username")
+        username: str = payload.get("name")
 
         if username is None:
             raise Exception("Token could not be validated")
-        return self.get_by_name(payload.get("username"))
+        return self.get_by_name(username)
 
 
-userService = UserService(database_engine)
+accountService = AccountService(database_engine)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse, Response
-from userservice import userService
+from accountservice import accountService
 
 import requestmodels, sso
 import os
@@ -23,47 +23,48 @@ async def login():
 @app.get("/auth_callback")
 async def auth_callback(request: Request):
     user = await sso.google_sso.verify_and_process(request)
-    local_user = userService.get_by_name(user.email)
-    if local_user is None:
-        local_user = userService.create(user.email)
-    userService.update_last_login(local_user)
+    account = accountService.get_by_name(user.email)
+    if account is None:
+        account = accountService.create(user.email)
+    accountService.update_last_login(account)
     response = RedirectResponse(url="/", status_code=302)
-    response.set_cookie('access_token', userService.create_access_token(user.email))
+    response.set_cookie('access_token', accountService.create_access_token(user.email))
     return response
 
 
 @app.middleware('http')
 async def auth_middleware(request: Request, call_next):
-    if os.environ.get('SKIP_AUTH') or request.url.path in ['/login', '/auth_callback']:
+    if os.environ.get('SKIP_AUTH') or request.url.path in ['/login', '/auth_callback','/openapi.json'] or request.url.path.startswith(
+            '/docs'):
         return await call_next(request)
 
-    user = userService.get_by_token(request.cookies.get('access_token'))
+    user = accountService.get_by_token(request.cookies.get('access_token'))
     if user is None:
         return Response(status_code=401)
     return await call_next(request)
 
 
 @app.get("/accounts")
-def read_users(limit: int = 10, offset: int = 0):
-    users = userService.get_all(limit, offset)
+def read_account(limit: int = 10, offset: int = 0):
+    users = accountService.get_all(limit, offset)
     return users
 
 
 @app.post("/accounts", status_code=201)
-def create_user(request: requestmodels.CreateUserRequest):
-    return userService.create(request.name)
+def create_account(request: requestmodels.CreateUserRequest):
+    return accountService.create(request.name)
 
 
-@app.get("/accounts/{user_id}")
-def get_user(user_id: int):
-    return userService.get(user_id)
+@app.get("/accounts/{account_id}")
+def get_account(account_id: int):
+    return accountService.get(account_id)
 
 
-@app.delete("/accounts/{user_id}", status_code=202)
-def delete_user(user_id: int):
-    userService.delete(user_id)
+@app.delete("/accounts/{account_id}", status_code=202)
+def delete_account(account_id: int):
+    accountService.delete(account_id)
 
 
-@app.put("/accounts/{user_id}", status_code=202)
-def update_user(user_id: int, request: requestmodels.CreateUserRequest):
-    userService.update(user_id=user_id, name=request.name)
+@app.put("/accounts/{account_id}", status_code=202)
+def update_account(account_id: int, request: requestmodels.CreateUserRequest):
+    accountService.update(account_id=account_id, name=request.name)
